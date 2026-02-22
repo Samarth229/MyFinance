a98ca3889dc43992e026b3f05c29ffcd4db6dd8e
Samarth <samarthkadam3411@gmail.com>
2026-02-22 22:45:47 +0530

    Implemented PersonRepository for CRUD operations

---
+++ lib/core/repositories/person_repository.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart';
+import '../database/database_helper.dart';
+import '../models/person.dart';
+
+class PersonRepository {
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  Future<int> insertPerson(Person person) async {
+    final db = await _databaseHelper.database;
+    return await db.insert('persons', person.toMap());
+  }
+
+  Future<List<Person>> getAllPersons() async {
+    final db = await _databaseHelper.database;
+    final result = await db.query('persons', orderBy: 'created_at DESC');
+    return result.map((map) => Person.fromMap(map)).toList();
+  }
+
+  Future<Person?> getPersonById(int id) async {
+    final db = await _databaseHelper.database;
+    final result =
+        await db.query('persons', where: 'id = ?', whereArgs: [id]);
+
+    if (result.isNotEmpty) {
+      return Person.fromMap(result.first);
+    }
+    return null;
+  }
+
+  Future<int> deletePerson(int id) async {
+    final db = await _databaseHelper.database;
+    return await db.delete('persons', where: 'id = ?', whereArgs: [id]);
+  }
+}
